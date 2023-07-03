f01fe1e7bb607076526a9d21a3f8d121241a4008
Dipali Bari <dipalibari333@gmail.com>
2023-07-03 12:38:02 +0530

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import './style.css'
+import './Header.css'
 
 function Header() {
   return (
-    <div id='nav'>
-        <a href='/'>HOME</a>
-        <a href='/contact'>CONTACT</a>
-        <a href='/service'>SERVICE</a>
-    </div>
-  )
+    <div className="nav"> 
+            <div className="logo">
+                <img src="https://classes.geekster.in/images/logo/Geekster.svg" alt="logo"/> 
+                
+            </div>
+            <div className="menu">
+                <a href="/">Home</a>
+                <a href="/contact">Contact</a>
+                <a href="/service">Srevice</a>
+                {/* <button onClick={(event)=>ChangeTheme(event)}>Change Theme</button> */}
+            </div>
+      </div>
+   
+   )
 }
 
 export default Header
